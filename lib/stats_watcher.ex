e408fe36ad284c29a9d306f80a4c7bc826600d82
cdimitroulas <c.dimitroulas@gmail.com>
2021-04-28 20:47:17 +0100

    Use users/by/username endpoint with query param instead

--- lib/stats_watcher.ex
+++ lib/stats_watcher.ex
@@ -1,8 +1,9 @@
 defmodule StatsWatcher do
   def mk_stats_url() do
     twitter_url = "https://api.twitter.com/2"
-    my_user_id = "806099443095404549"
-    user_followers_endpoint = "/users/" <> my_user_id <> "/followers"
-    twitter_url <> user_followers_endpoint
+    my_username = "chriswearshats"
+    users_endpoint = "/users/by/username/" <> my_username
+    query_params = "?user.fields=public_metrics"
+    twitter_url <> users_endpoint <> query_params
   end
 end
--- test/stats_watcher_test.exs
+++ test/stats_watcher_test.exs
@@ -3,7 +3,8 @@ defmodule StatsWatcherTest do
 
   describe "StatsWatcher.mk_stats_url" do
     test "returns the correct url" do
-      assert StatsWatcher.mk_stats_url() == "https://api.twitter.com/2/users/806099443095404549/followers"
+      assert StatsWatcher.mk_stats_url() ==
+        "https://api.twitter.com/2/users/by/username/chriswearshats?user.fields=public_metrics"
     end
   end
 end
